e6d82241ebca5a702989cac7a25b27f90b5a3d6d
CANH8 <vietcanhm10@gmail.com>
2026-09-02 21:22:49 +0700

    update lại ( triển khai CRUD và vòng lặp an toàn )

--- ContactManager.cpp
+++ ContactManager.cpp
@@ -56,3 +56,44 @@ bool ContactManager::saveToFile(const string& f) const
     }
     return true;
 }
+bool ContactManager::addContact(const Contact& c) 
+{
+    if (!Person::isValidPhone(c.getPhone())) return false; 
+    if (hasPhone(c.getPhone())) return false;              
+    contacts.push_back(c);
+    return true;
+}
+void ContactManager::displayAll() const 
+{
+    for (size_t i = 0; i < contacts.size(); i++) 
+    {
+        const Person& p = contacts[i];
+        cout << i + 1 << ". ";
+        p.display();
+    }
+}
+bool ContactManager::updateByPhone(const string& phone, const string& newName, const string& newRel) 
+{
+    Contact key("", phone); 
+    vector<Contact>::iterator it = find(contacts.begin(), contacts.end(), key);
+    if (it == contacts.end()) return false;
+    if (!newName.empty()) it->setName(newName); 
+    if (!newRel.empty())  it->setRelationship(newRel);
+    return true;
+}
+int ContactManager::deleteByName(const string& name) 
+{
+    int count = 0;
+    for (auto it = contacts.begin(); it != contacts.end(); ) 
+    {
+        if (toLower(it->getName()) == toLower(name)) 
+        {
+            it = contacts.erase(it);
+            count++;
+        }
+        else {
+            ++it; 
+        }
+    }
+    return count;
+}
